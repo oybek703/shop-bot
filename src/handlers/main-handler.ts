d2a7a9a3602e9bc6c7cf77f9a9c0ff390b408cec
Yoriqulov Oybek <hhoybek@gmail.com>
2022-12-13 15:08:57 +0500

    start and help command

--- src/handlers/main-handler.ts
+++ src/handlers/main-handler.ts
@@ -1,16 +1,40 @@
-import { Telegraf } from 'telegraf'
+import { Context, Telegraf } from 'telegraf'
 import { DBManager } from '../database/db-manager'
 
 export class Handler {
   private readonly bot: Telegraf
   private readonly dbManager: DBManager
+  private chatId: number | undefined
 
   async init(dbManager: DBManager, bot: Telegraf) {
+    bot.start(this.start)
+    bot.help(this.help)
     await bot.telegram.setMyCommands([
       { command: 'start', description: 'Start shop bot.' },
       { command: 'help', description: 'Get help from bot.' }
     ])
   }
 
-  async handle() {}
+  async start(ctx: Context) {
+    const chatId = ctx.message?.chat.id
+    const firstName = ctx.message?.from.first_name
+    const lastName = ctx.message?.from.last_name
+    if (chatId) {
+      await ctx.telegram.sendMessage(chatId, `Hi ${firstName || ''} ${lastName || ''} 🖐`, {
+        reply_markup: {
+          keyboard: [
+            [{ text: '🛍 Select product' }],
+            [{ text: 'ℹ About shop bot' }, { text: '⚙ Settings' }]
+          ]
+        }
+      })
+    }
+  }
+
+  async help(ctx: Context) {
+    const chatId = ctx.message?.chat.id
+    if (chatId) {
+      await ctx.telegram.sendMessage(chatId, 'Shop bot 🤖 will help you buy products online🙂')
+    }
+  }
 }
